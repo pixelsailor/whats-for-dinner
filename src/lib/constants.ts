b03789013b85d6799f1af9d3a4a1dba5dac9ec27
pixelsailor <smoov.b@gmail.com>
2025-06-26 11:16:10 -0500

    add IconButton component

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const MIN_DESKTOP_SIZE = 1024;
+
+// COMPONENT SPECS. USES MATERIAL GUIDLINES AND CONVERTED FOR TW SPACING (material_dp/4)
+export const TOOLBAR_HEIGHT = 16;
+export const TOOLBAR_HEIGHT_PX = 64;
+export const TOOLBAR_PADDING = 4; // only applied to left and right
+export const TOOLBAR_MAX_ACTION_GAP = 8;
+export const TOOLBAR_MIN_ACTION_GAP = 1;
+export const FLOATING_TOOLBAR_MARGIN = 4;
+export const FLOATING_TOOLBAR_PADDING = 2;
+
+export const BUTTON_MIN_TARGET_HEIGHT = 12;
+
+export const LIST_ITEM_HEIGHT = 14;
+// left/right padding is 4(16px) but should be applied via the fist and last children in the list item container
+export const LIST_ITEM_PADDING = 2; // top/bottom only
---
+++ src/lib/ui/IconButton.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { BUTTON_MIN_TARGET_HEIGHT } from '$lib/constants';
+	import type { MouseEventHandler } from 'svelte/elements';
+
+	type IconButtonProps = {
+		size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+		onClick: MouseEventHandler<HTMLButtonElement>;
+		children: any;
+		[x: string]: any;
+	};
+
+	let { size = 'sm', onClick, children, ...props }: IconButtonProps = $props();
+
+	let iconSize = $derived.by(() => {
+    const sizes = {
+      xs: 16,
+      sm: 20,
+      md: 24,
+      lg: 32,
+      xl: 40
+    };
+    return `${sizes[size]}px`;
+	});
+</script>
+
+<button
+	class={['button icon-button', props.class]}
+	style:min-width={`${BUTTON_MIN_TARGET_HEIGHT / 4}rem`}
+	style:height={`${BUTTON_MIN_TARGET_HEIGHT / 4}rem`}
+	style={props.style}
+	onclick={onClick}
+	disabled={props.disabled}
+	title={props.title}
+>
+	<span class="icon-button__icon" style:width={iconSize} style:height={iconSize}>
+		{@render children()}
+	</span>
+	{#if props.label}
+		<span class="sr-only">{props.label}</span>
+	{/if}
+</button>
+
+<style>
+	.icon-button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.icon-button:disabled {
+		opacity: 0.7;
+	}
+
+	.icon-button__icon {
+		display: flex;
+	}
+</style>

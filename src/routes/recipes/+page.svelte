b89710dd4e2661d767f187e45300b26d7006f7ef
pixelsailor <smoov.b@gmail.com>
2025-06-26 17:40:04 -0500

    add toast for undo delete

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,28 +1,35 @@
 <script lang="ts">
 	import { Toolbar } from 'bits-ui';
+	import { toast, Toaster } from 'svelte-sonner';
 
 	import { db } from '$lib/db';
 	import { savedRecipes } from '$lib/stores/recipes';
-  import { List, ListItem } from '$lib/ui/List';
+	import { List, ListItem } from '$lib/ui/List';
 	import IconButton from '$lib/ui/IconButton.svelte';
 	import TrashIcon from '$lib/ui/Icons/TrashIcon.svelte';
+	import ProgressSpinner from '$lib/ui/ProgressSpinner.svelte';
+	import { slide } from 'svelte/transition';
 
-  let alert = $state({
-    type: '' as 'info' | 'warn' | 'danger' | 'success' | 'error',
-    message: ''
-  });
+	async function deleteRecipe(id: string, title: string) {
+		// if (confirm('Delete this recipe?')) {
+		try {
+			await db.recipes.update(id, { archived: true });
 
-  async function deleteRecipe(id: string) {
-    if (confirm('Delete this recipe?')) {
-      try {
-        await db.recipes.update(id, { archived: true });
-      } catch (err) {
-        alert.type = 'error';
-        alert.message = 'Failed to delete recipe';
-        console.error(err);
-      }
-    }
-  }
+			toast.success(`"${title}" deleted`, {
+				action: {
+					label: 'Undo',
+					onClick: async () => {
+						await db.recipes.update(id, { archived: false });
+						toast.success(`"${title}" restored`);
+					}
+				}
+			});
+		} catch (err) {
+			toast.error('Failed to delete recipe');
+			console.error(err);
+		}
+		// }
+	}
 </script>
 
 <header>
@@ -30,31 +37,34 @@
 		<Toolbar.Link href="/">
 			<span>Back</span>
 		</Toolbar.Link>
-    <Toolbar.Link href="/recipes/trash">
-      <span>Trash bin</span>
-    </Toolbar.Link>
+		<Toolbar.Link href="/recipes/trash">
+			<span>Trash bin</span>
+		</Toolbar.Link>
 	</Toolbar.Root>
 </header>
 <main>
 	<h1 class="m-4 text-2xl font-bold">My Recipe Book</h1>
-
-	{#if $savedRecipes.length > 0}
+	{#if $savedRecipes.length === 0}
+		<p>You haven't saved any recipes yet.</p>
+	{:else if $savedRecipes.length > 0}
 		<List size="three-line">
 			{#each $savedRecipes as recipe}
-        <ListItem.Root>
-          <ListItem.Link href="/recipes/{recipe.id}">
-            <ListItem.Text primary={recipe.title} secondary={recipe.short_description} />
-          </ListItem.Link>
-          <ListItem.SecondaryAction>
-            <IconButton onClick={() => deleteRecipe(recipe.id)} size="xs">
-              <TrashIcon />
-            </IconButton>
-          </ListItem.SecondaryAction>
-        </ListItem.Root>
+				<ListItem.Root>
+					<ListItem.Link href="/recipes/{recipe.id}">
+						<ListItem.Text primary={recipe.title} secondary={recipe.short_description} />
+					</ListItem.Link>
+					<ListItem.SecondaryAction>
+						<IconButton onClick={() => deleteRecipe(recipe.id, recipe.title)} size="xs">
+							<TrashIcon />
+						</IconButton>
+					</ListItem.SecondaryAction>
+				</ListItem.Root>
 			{/each}
 		</List>
 	{:else}
-		<p>You haven't saved any recipes yet.</p>
+		<div class="absolute inset-0 grid place-content-center">
+			<ProgressSpinner size="lg" />
+		</div>
 	{/if}
 </main>
 <footer></footer>

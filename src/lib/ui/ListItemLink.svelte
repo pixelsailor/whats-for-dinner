9ff71a0501a1afa60eab2844d5132a5c5964ddb6
pixelsailor <smoov.b@gmail.com>
2025-06-25 21:48:46 -0500

    add UI components

---
+++ src/lib/ui/ListItemLink.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	type ListItemLinkProps = {
+		children: any;
+		href: string;
+    size?: 'two-line' | 'three-line';
+		[x: string]: any;
+	};
+	let { children, href, size = 'two-line', ...props }: ListItemLinkProps = $props();
+</script>
+
+<li class={[{'h-18': size === 'two-line'}, {'h-22': size === 'three-line'}]}>
+	<a
+		{...props}
+		href={href}
+		class={['h-full w-full py-2 text-left truncate', {'text-wrap': size === 'three-line'}, props.class]}
+	>
+		{@render children()}
+	</a>
+</li>
+
+<style>
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+</style>
---
+++ src/lib/ui/ProgressSpinner.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  type SpinnerProps = {
+    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  }
+  let { size = 'sm' }: SpinnerProps = $props();
+
+  let pixelSize = $derived.by(() => {
+    const sizes = {
+      xs: 20,
+      sm: 24,
+      md: 24,
+      lg: 32,
+      xl: 40,
+    }
+    return `${sizes[size]}px`;
+  })
+</script>
+
+<span class="pxl--progress-spinner">
+  <svg width={pixelSize} height={pixelSize} viewBox="0 0 100 100">
+    <circle class="bg" cx="50" cy="50" r="38" />
+    <circle class="loader" cx="50" cy="50" r="38" />
+  </svg>
+</span>
+
+<style>
+  .bg {
+    fill: none;
+    stroke-width: 8px;
+    stroke: white;
+  }
+  .loader {
+    fill: none;
+    stroke-width: 8px;
+    stroke: black;
+    stroke-dasharray: 288;
+    transform-origin: 50% 50%;
+    animation: line 5s linear infinite, spin 3s linear infinite;
+  }
+
+  @keyframes line {
+    0% {
+      stroke-dashoffset: 1152;
+    }
+
+    100% {
+      stroke-dashoffset: 576;
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(-90deg) translate3d(0, 0, 0);
+    }
+
+    100% {
+      transform: rotate(270deg) translate3d(0, 0, 0);
+    }
+  }
+</style>

7d4f614ff4825c43885e0ecab4fb27c70f5a9de1
pixelsailor <smoov.b@gmail.com>
2025-06-29 23:29:16 -0500

    add Panel component

---
+++ src/lib/ui/Panel.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { ClassValue } from 'svelte/elements';
+
+	type PanelProps = {
+		bordered?: boolean;
+		children: any;
+		class?: ClassValue;
+		shadow?: boolean;
+	};
+	let { bordered, children, shadow, ...props }: PanelProps = $props();
+</script>
+
+<div
+	class={[
+		'p-3 rounded-lg border border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-gray-900 shadow-xs',
+		props.class
+	]}
+>
+	{@render children()}
+</div>

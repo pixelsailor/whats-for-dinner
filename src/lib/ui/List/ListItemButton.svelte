a6d10a5e1099f754c80485063d79334c1d066b7c
pixelsailor <smoov.b@gmail.com>
2025-07-02 13:08:46 -0500

    fix: add disabled state to ListItemButton

--- src/lib/ui/List/ListItemButton.svelte
+++ src/lib/ui/List/ListItemButton.svelte
@@ -4,10 +4,11 @@
 
 	type ListItemLinkProps = {
 		children: any;
+		disabled?: boolean;
 		onClick: MouseEventHandler<HTMLButtonElement>;
 		label?: string;
 	};
-	let { children, onClick, label, ...props }: ListItemLinkProps = $props();
+	let { children, disabled, onClick, label, ...props }: ListItemLinkProps = $props();
 
 	let size = getContext('pxl-list');
 </script>
@@ -15,12 +16,14 @@
 <button
 	{...props}
 	class={[
-		'pxl-listitem__link flex h-full w-full flex-row items-center gap-3 pl-4',
+		'pxl-listitem__link flex h-full w-full flex-row items-center gap-3 pl-4 hover:cursor-pointer',
 		{ 'place-content-center gap-4': size === 'two-line' },
-		{ 'place-content-start gap-4': size === 'three-line' }
+		{ 'place-content-start gap-4': size === 'three-line' },
+		{ 'opacity-60': disabled }
 	]}
 	onclick={onClick}
 	aria-label={label}
+	{disabled}
 >
 	{@render children()}
 </button>

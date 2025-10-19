9d49928c151bd2f1ee023182a96fc7ef96a2b2bc
pixelsailor <smoov.b@gmail.com>
2025-10-19 17:34:03 -0500

    add ViewedBadge

---
+++ src/lib/ui/ViewedBadge.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { viewed }: { viewed: boolean } = $props();
+</script>
+
+{#if viewed}
+	<span class="inline-flex items-center px-2 py-1 text-xs font-medium text-gray-500 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-gray-400">
+		Viewed
+	</span>
+{/if}

44647064becd09448de67c30b2e23d5743557186
pixelsailor <smoov.b@gmail.com>
2025-11-27 09:05:26 -0600

    update login page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,12 +1,24 @@
-<form method="POST" action="?/login">
-  <label>
-    Email
-    <input name="email" type="email" />
-  </label>
-  <label>
-    Password
-    <input name="password" type="password" />
-  </label>
-  <button>Login</button>
-  <!-- <button formaction="?/signup">Sign up</button> -->
-</form>
+<script lang="ts">
+  // import { Section, Register } from "flowbite-svelte-blocks";
+  import { Button, Card, Checkbox, Label, Input } from "flowbite-svelte";
+</script>
+
+<div class="flex justify-center items-center h-screen">
+  <Card class="p-4 sm:p-6 md:p-8 w-96">
+    <form method="POST" action="?/login" class="flex flex-col space-y-6">
+      <h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Log In</h3>
+      <Label class="space-y-2">
+        <span>Email</span>
+        <Input type="email" name="email" placeholder="name@company.com" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Password</span>
+        <Input type="password" name="password" placeholder="•••••" required />
+      </Label>
+      <Button type="submit" class="w-full1">Sign in</Button>
+      <!-- <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+        Don’t have an account yet? <a href="/" class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a>
+      </p> -->
+    </form>
+  </Card>
+</div>

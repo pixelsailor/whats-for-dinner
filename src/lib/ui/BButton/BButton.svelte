c9d3e07e6a887fe45dba5e1a6ab59e4943c222af
pixelsailor <smoov.b@gmail.com>
2025-11-29 10:10:15 -0600

    add recipe lock icons and button

---
+++ src/lib/ui/BButton/BButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button } from 'bits-ui';
+
+	type BButtonProps = {
+		children: any;
+		[x: string]: any;
+	};
+
+	let { children, class: className, ...props }: BButtonProps = $props();
+</script>
+
+<Button.Root
+  class="rounded-input bg-dark text-background shadow-mini hover:bg-dark/95 inline-flex
+  h-12 items-center justify-center px-[21px] text-[15px]
+  font-semibold active:scale-[0.98] active:transition-all"
+>
+  {@render children()}
+</Button.Root>

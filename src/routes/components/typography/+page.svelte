5d91c5c1f654a8284b8247f818fd8f43705655d3
pixelsailor <smoov.b@gmail.com>
2025-12-30 15:38:56 -0600

    add material type styles and demo

---
+++ src/routes/components/typography/+page.svelte
@@ -0,0 +1,62 @@
+<article class="mx-auto max-w-5xl px-4 py-8 lg:px-8 flex flex-col gap-4">
+  <div>
+    <h1 class="display-large">Display Large</h1>
+    <p class="label-medium text-accent"><code>.display-large</code></p>
+  </div>
+  <div>
+    <h1 class="display-medium">Display Medium</h1>
+    <p class="label-medium"><code>.display-large</code></p>
+  </div>
+  <div>
+    <h1 class="display-small">Display Small</h1>
+    <p class="label-medium"><code>.display-small</code></p>
+  </div>
+  <div>
+    <h1 class="headline-large">Headline Large</h1>
+    <p class="label-medium"><code>.headline-large</code></p>
+  </div>
+  <div>
+    <h1 class="headline-medium">Headline Medium</h1>
+    <p class="label-medium"><code>.headline-medium</code></p>
+  </div>
+  <div>
+    <h1 class="headline-small">Headline Small</h1>
+    <p class="label-medium"><code>.headline-small</code></p>
+  </div>
+  <div>
+    <h1 class="title-large">Title Large</h1>
+    <p class="label-medium"><code>.title-large</code></p>
+  </div>
+  <div>
+    <h1 class="title-medium">Title Medium</h1>
+    <p class="label-medium"><code>.title-medium</code></p>
+  </div>
+  <div>
+    <h1 class="title-small">Title Small</h1>
+    <p class="label-medium"><code>.title-small</code></p>
+  </div>
+  <div>
+    <h1 class="body-large">Body Large</h1>
+    <p class="label-medium text-muted"><code>.body-large</code></p>
+  </div>
+  <div>
+    <h1 class="body-medium">Body Medium</h1>
+    <p class="label-medium"><code>.body-medium</code></p>
+  </div>
+  <div>
+    <h1 class="body-small">Body Small</h1>
+    <p class="label-medium"><code>.body-small</code></p>
+  </div>
+  <div>
+    <h1 class="label-large">Label Large</h1>
+    <p class="label-medium"><code>.label-large</code></p>
+  </div>
+  <div>
+    <h1 class="label-medium">Label Medium</h1>
+    <p class="label-medium"><code>.label-medium</code></p>
+  </div>
+  <div>
+    <h1 class="label-small">Label Small</h1>
+    <p class="label-medium"><code>.label-small</code></p>
+  </div>
+</article>
